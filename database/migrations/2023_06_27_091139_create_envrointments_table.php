4ee8d0caee181286790479fa0c631b6da8c3013a
dzaki236 <dzakiahnafz@gmail.com>
2023-06-30 20:43:13 +0700

    make new field for availables

--- database/migrations/2023_06_27_091139_create_envrointments_table.php
+++ database/migrations/2023_06_27_091139_create_envrointments_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->longText('desc');
             $table->string('logo');
+            $table->enum('available', ['guest', 'member'])->default('guest');
             $table->timestamps();
         });
     }
