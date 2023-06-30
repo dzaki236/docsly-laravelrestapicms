d6ff22f6c9e89f5dd20090133e97c6e1a6afb6d6
dzaki236 <dzakiahnafz@gmail.com>
2023-07-01 02:38:00 +0700

    i dont know what im write in here

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
 
-Auth::routes();
+Auth::routes(['register'=>false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/docs', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
